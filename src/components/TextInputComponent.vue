031089e7cf8467c63538327e3e9655150ee7426e
riya09 <faria.ayir@gmail.com>
2025-05-10 22:56:00 +0600

    feat: add new button to generate empty page

--- src/components/TextInputComponent.vue
+++ src/components/TextInputComponent.vue
@@ -1,45 +1,61 @@
 <template>
   <div class="chatbox-wrapper">
-    <div class="chat-count">
+    <div class="chat-header">
       <button
-        class="arrows"
-        @click="$emit('setCurrentHistory', -1)"
+        class="new-chat-btn"
+        :disabled="isLoading"
+        aria-label="New Chat"
+        @click="$emit('newChat')"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-arrow-left"
-        >
-          <line x1="19" y1="12" x2="5" y2="12"></line>
-          <polyline points="12 5 5 12 12 19"></polyline>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+          <path fill="#fff" d="M320 464c8.8 0 16-7.2 16-16l0-288-80 0c-17.7 0-32-14.3-32-32l0-80L64 48c-8.8 0-16
+          7.2-16 16l0 384c0 8.8 7.2 16 16 16l256 0zM0 64C0 28.7 28.7 0 64 0L229.5 0c17 0 33.3 6.7
+          45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3L384 448c0 35.3-28.7 64-64 64L64 512c-35.3
+          0-64-28.7-64-64L0 64z"/>
         </svg>
+        <span>New</span>
       </button>
-      <span>{{ (store.currentChatIndex + 1) }} / {{ store.chatHistory.length || 1 }}</span>
-      <button
-        class="arrows"
-        @click="$emit('setCurrentHistory', 1)"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-arrow-right"
+      <div class="chat-count">
+        <button
+          class="arrows"
+          @click="$emit('setCurrentHistory', -1)"
         >
-          <line x1="5" y1="12" x2="19" y2="12"></line>
-          <polyline points="12 19 19 12 12 5"></polyline>
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-arrow-left"
+          >
+            <line x1="19" y1="12" x2="5" y2="12"></line>
+            <polyline points="12 5 5 12 12 19"></polyline>
+          </svg>
+        </button>
+        <span>{{ (store.currentChatIndex + 1) }} / {{ store.chatHistory.length || 1 }}</span>
+        <button
+          class="arrows"
+          @click="$emit('setCurrentHistory', 1)"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-arrow-right"
+          >
+            <line x1="5" y1="12" x2="19" y2="12"></line>
+            <polyline points="12 19 19 12 12 5"></polyline>
+          </svg>
+        </button>
+      </div>
     </div>
     <div
       v-if="store.chatHistory.length <= 1"
